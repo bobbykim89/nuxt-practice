36ef7322ec3452647c396f8125a2858d6b5bd2b8
bobbykim89 <bobby.sigun.kim@gmail.com>
2021-12-26 20:43:48 -0700

    updated loading animation and fetch single data from api

--- pages/index.vue
+++ pages/index.vue
@@ -9,8 +9,11 @@
       <button v-show="searchInput !== ''" class="button" @click="clearSearch">Clear Search</button>
     </div>
 
+    <!-- Loading -->
+    <Loading v-if="$fetchState.pending" />
+
     <!-- Movies -->
-    <div class="container movies">
+    <div v-else class="container movies">
       <!-- Searched Movies -->
       <div v-if="searchInput !== ''" id='movie-grid' class="movies-grid">
         <div v-for="(movie, index) in searchedMovies" :key='index' class="movie">
@@ -99,6 +102,7 @@ export default {
     }
     await this.searchMovies()
   },
+  fetchDelay: 1000,
   methods: {
     async getMovies() {
       const apiKey = process.env.API_KEY
---
+++ pages/movies/_movieid.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container single-movie"></div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'single-movie',
+  data() {
+    return {
+      movie: null,
+    }
+  },
+  async fetch() {
+    await this.getSingleMovie()
+  },
+  methods: {
+    async getSingleMovie() {
+      const apiKey = process.env.API_KEY
+      const data = axios.get(
+        `https://api.themoviedb.org/3/movie/${this.$route.params.movieid}?api_key=${apiKey}&language=en-US`
+      )
+      const result = await data
+      this.movie = result.data
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
